5ff2324261c9a33dc1e2aab2a5864a3770535a6a
beka-tech <abeka039@gmail.com>
2022-12-20 15:15:29 +0300

    todo app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import React from 'react';
+import React,{ useState }from 'react';
 import "./App.css";
 import Form from './components/Form';
+import TodoList from './components/TodoList';
 
 function App() {
+  const [inputText, setInputText] = useState("");
   return (
     <div className="App">
-   <p></p>
-   <Form/>
+      <header>
+      <h1>BEKA Todo List</h1>
+        </header>
+   <Form setInputText={setInputText} />
+   <TodoList inputText={inputText} />
     </div>
   );
 }
---
+++ src/components/TodoList.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const TodoList = ({setInputText}) => {
+    return (
+        <div className="todo-container">
+            <ul  class="todo-list"> 
+            <li  > 1</li>
+            </ul>
+            </div>
+    );
+};
+
+export default TodoList;
